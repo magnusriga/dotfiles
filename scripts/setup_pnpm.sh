e1dd61cb34fc8910313dff1eeda59379d15be66a
Magnus <magnus.rinnan@gmail.com>
2025-06-12 00:07:03 +0200

    fix: updated pnpm packages

--- scripts/setup_pnpm.sh
+++ scripts/setup_pnpm.sh
@@ -12,28 +12,25 @@ case ":$PATH:" in
 *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 
-# Install global pnpm packages.
-pnpm add -g node-gyp node-gyp-build \
-  neovim \
-  yarn \
-  mcp-hub@latest
-
 pnpm --allow-build=spawn-sync \
   --allow-build=tree-sitter \
   --allow-build=@mistweaverco/tree-sitter-graphql \
   --allow-build=@mistweaverco/tree-sitter-kulala \
   --allow-build=contentful-cli \
+  --allow-build=puppeteer \
+  --allow-build=vue-demi \
+  --allow-build=@anthropic-ai/claude-code \
+  --allow-build=yarn \
   add -g \
   contentful-cli \
   neonctl \
-  @mistweaverco/kulala-ls
-
-pnpm --allow-build=puppeteer \
-  --allow-build=vue-demi \
-  add -g \
-  @mermaid-js/mermaid-cli
-
-pnpm --allow-build=@anthropic-ai/claude-code add -g @anthropic-ai/claude-code
+  @mistweaverco/kulala-ls \
+  node-gyp node-gyp-build \
+  neovim \
+  yarn \
+  mcp-hub@latest \
+  @mermaid-js/mermaid-cli \
+  @anthropic-ai/claude-code
 
 # pnpm --allow-build=tree-node-cli add -g tree-node-cli
 # pnpm --allow-build=contentful-cli add -g contentful-cli
