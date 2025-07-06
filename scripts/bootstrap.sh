1fe014a5aaadc09f2e9b473a0f529ff1f2109c32
Magnus <magnus.rinnan@gmail.com>
2025-07-06 22:35:03 +0200

    foobar

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -83,10 +83,7 @@ function doIt() {
   # ==========================================================
   # Save top-level `dotfiles` path.
   # ==========================================================
-  ROOTPATH="$(
-    cd -- "$(dirname "${BASH_SOURCE[0]}").." >/dev/null 2>&1 || return
-    pwd -P
-  )"
+  ROOTPATH="$(cd -- "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd -P)"
   echo "ROOTPATH is $ROOTPATH."
 
   # ==========================================================
